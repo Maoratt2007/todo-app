0edeef55df4d1c3f5ffef1c5531e3a9d0c2358e0
Maor Attias <maoratt2007@gmail.com>
2025-08-14 00:07:10 +0300

    create prompts folder+ do magicLinkComponent

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
+//createClient is a function that allows us use the functions of supabase in our code 
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    //import the url and the anon key from the .env file
-  import.meta.env.VITE_SUPABASE_URL!, 
-  import.meta.env.VITE_SUPABASE_ANON_KEY!    
+  //import the url and the anon key from the .env file
+  //vite scan all the env variables and make them available to the code he switches from import.meta.env to environment variables
+  import.meta.env.VITE_SUPABASE_URL!, //! says that the url is not null
+  import.meta.env.VITE_SUPABASE_ANON_KEY! //! says that the anon key is not null
 );
