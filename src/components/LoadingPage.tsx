fbfef8d7041d574b90f35b97c3bd477b740ad191
Maor Attias <maoratt2007@gmail.com>
2025-08-19 13:15:47 +0300

    fix loadPage- prevents two tab with todoPage

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,9 +1,11 @@
 // src/LoadPage.tsx
-import { useEffect, useRef } from "react";
+import { useEffect, useRef,useState } from "react";
 import { supabase } from "../lib/supabase";
 
 export default function LoadPage() {
   const didRun = useRef(false);//valuable that dont render when he changed 
+  const [err, setErr] = useState<string | null>(null);
+  const [signedIn, setSignedIn] = useState(false);//flag to if we have session or not yet
 
   useEffect(() => {
     if (didRun.current) return; // avoid two useEffect run from strictMode in react
@@ -25,18 +27,38 @@ export default function LoadPage() {
         }
 
         //if there is a session go to app and he decide todopage or authpage else /login 
-        window.location.replace(session ? "/" : "/login");// "/"" go to <App />
-      } catch {
+        if (session) {
+          //if already connected we dont want open new page with todopage
+          setSignedIn(true);
+        } else {
+          // if not connected back to login
+          window.location.replace("/login");
+        }
+      } catch(e) {
         // clean the url and back to /login 
         window.history.replaceState({}, document.title, window.location.pathname);
-        window.location.replace("/login");
+        setErr(e instanceof Error ? e.message : "Unexpected error");//if there is an error use it else write "Unexpected error"
       }
     })(); //run this function 
   }, []);
 
   return (
     <div style={{ maxWidth: 420, margin: "80px auto", textAlign: "center" }}>
-      <h1>Connecting…</h1>
+      {err ? (
+        <>
+          <h1 style={{ color: "crimson" }}>There is an error</h1>
+          <p>{err}</p>
+          <p><a href="/login">back to login page</a></p>
+        </>
+      ) : signedIn ? (
+        <>
+          <h1>The todopage already opened </h1>
+          <p>Back to your first tab that opened </p>
+        </>
+      ) : (
+        <h1>Connecting…</h1>
+      )}
     </div>
   );
+
 }
